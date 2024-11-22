3598749274ff47e0d66b3865358e23dce1bc57ce
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 08:32:28 +0700

    Menambahkan fungsi RencanaStudyView untuk tampilan rencana studi beserta dengan Data mahasiswa yang akan digunakan dalam tampilan, Callback untuk menangani aksi tombol simpan dan Callback untuk menangani aksi tombol kembali

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
@@ -3,3 +3,11 @@ package com.example.session8_navigationmultipledata.ui.view.screen
 import androidx.compose.runtime.Composable
 import com.example.session8_navigationmultipledata.model.Mahasiswa
 
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}
