960ff4f42189165bfee2da6e669fcee731390f67
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 09:49:31 +0700

    Memperbaiki beberapa kesalahan code pada file DynamicSelectedField.kt

--- app/src/main/java/com/example/session8_navigationmultipledata/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/widget/DynamicSelectedField.kt
@@ -1,23 +1,27 @@
 package com.example.session8_navigationmultipledata.widget
 
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import java.lang.reflect.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectTextField(
     selectedValue: String,
     options: List<String>,
     label: String,
     onValueChangedEvent: (String) -> Unit,
-    modifier: Modifier = modifier
+    modifier: Modifier
 ){
     var expanded by remember { mutableStateOf(false) }
 
