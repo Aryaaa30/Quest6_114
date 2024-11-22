36c3c8f8ccc196de1c6c23a66aad1c13d3781332
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 09:03:26 +0700

    Menambahkan kolom dengan teks dan spacer untuk instruksi mata kuliah peminatan

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
@@ -90,6 +90,19 @@ fun RencanaStudyView(
             )
                 .fillMaxSize()
         ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ){
+                Text(text = "Pilih Mata Kuliah Pemintan", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
 
         }
     }
