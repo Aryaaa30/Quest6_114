2ba4f8b67b6ca752b5a65d0fef67fa6f7d05bbfd
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 13:03:32 +0700

    Menambahkan tampilan foto mahasiswa menggunakan komponen Image dan layout dengan Column

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/TampilanDataMahasiswaView.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/TampilanDataMahasiswaView.kt
@@ -1,7 +1,23 @@
 package com.example.session8_navigationmultipledata.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.session8_navigationmultipledata.R
 import com.example.session8_navigationmultipledata.model.Mahasiswa
 
 @Composable
@@ -9,5 +25,21 @@ fun TampilanDataMahasiswaView(
     mhs: Mahasiswa,
     navHostController: NavHostController
 ){
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.arya),
+            contentDescription = "Foto Mahasiswa",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+    }
+
 
 }
