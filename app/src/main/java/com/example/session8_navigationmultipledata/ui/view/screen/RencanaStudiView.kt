ee246e63b190757156f53aa4f951ca017f3a7bcc
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 08:30:33 +0700

    Menambahkan file RencanaStudiView.kt

---
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
@@ -0,0 +1,5 @@
+package com.example.session8_navigationmultipledata.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.session8_navigationmultipledata.model.Mahasiswa
+
