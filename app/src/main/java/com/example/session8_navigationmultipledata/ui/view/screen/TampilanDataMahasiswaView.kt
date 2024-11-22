f07c8764cf46850291769317d222073770df1be1
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 12:59:12 +0700

    Menambahkan komponen TampilanDataMahasiswaView untuk menampilkan data mahasiswa

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/TampilanDataMahasiswaView.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/TampilanDataMahasiswaView.kt
@@ -1,2 +1,13 @@
 package com.example.session8_navigationmultipledata.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.example.session8_navigationmultipledata.model.Mahasiswa
+
+@Composable
+fun TampilanDataMahasiswaView(
+    mhs: Mahasiswa,
+    navHostController: NavHostController
+){
+
+}
