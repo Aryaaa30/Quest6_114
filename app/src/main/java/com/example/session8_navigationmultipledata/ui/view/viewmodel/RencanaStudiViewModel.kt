17ee46da00a6c7b1d63f116999fc9eeb67daf0ce
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 10:34:59 +0700

    Menambahkan fungsi setMataKuliah untuk memperbarui nama mata kuliah dalam RencanaStudi

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.session8_navigationmultipledata.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.session8_navigationmultipledata.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,5 +10,9 @@ import kotlinx.coroutines.flow.update
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
 
 }
