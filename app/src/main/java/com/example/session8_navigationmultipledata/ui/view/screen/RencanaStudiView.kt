8ecc112b992baff7134d03ab1a8455efe01cae2c
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 08:50:53 +0700

    Menambahkan gambar dengan bentuk lingkaran dan ukuran 50dp

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
@@ -1,17 +1,22 @@
 package com.example.session8_navigationmultipledata.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.session8_navigationmultipledata.R
 import com.example.session8_navigationmultipledata.model.Mahasiswa
@@ -39,9 +44,14 @@ fun RencanaStudyView(
                 .fillMaxWidth()
                 .padding(16.dp), verticalAlignment = Alignment.CenterVertically
         ){
-
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
         }
-
     }
 
 }
