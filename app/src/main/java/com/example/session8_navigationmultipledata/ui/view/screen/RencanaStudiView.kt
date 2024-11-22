a8dfbb9f47d0c2d943c7a24c07a7b0d36586177e
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 08:46:42 +0700

    Menambahkan komponen Column dengan latar belakang warna primary

--- app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/ui/view/screen/RencanaStudiView.kt
@@ -1,8 +1,14 @@
 package com.example.session8_navigationmultipledata.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.session8_navigationmultipledata.R
 import com.example.session8_navigationmultipledata.model.Mahasiswa
 
 @Composable
@@ -17,5 +23,13 @@ fun RencanaStudyView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.Primary))
+    ){
+
+    }
 
 }
