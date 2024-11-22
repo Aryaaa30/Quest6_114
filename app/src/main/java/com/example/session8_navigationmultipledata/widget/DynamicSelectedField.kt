51f2ada9da68700736cb265954741680c383fb8a
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2024-11-22 09:11:56 +0700

    Menambahkan fungsi DynamicSelectTextField untuk input dinamis dengan opsi pilihan

--- app/src/main/java/com/example/session8_navigationmultipledata/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/session8_navigationmultipledata/widget/DynamicSelectedField.kt
@@ -1,2 +1,15 @@
 package com.example.session8_navigationmultipledata.widget
 
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
